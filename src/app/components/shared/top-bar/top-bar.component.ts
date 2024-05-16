f978d9f1eced7307d2b99aa04c573e59dc3e9b89
OtavioAugustto <otavioaugustto93@gmail.com>
2024-05-16 19:40:15 -0300

    Creating top bar component

--- src/app/components/shared/top-bar/top-bar.component.ts
+++ src/app/components/shared/top-bar/top-bar.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { Route, Router } from '@angular/router';
 
 @Component({
   selector: 'app-top-bar',
   standalone: true,
   imports: [],
   templateUrl: './top-bar.component.html',
-  styleUrl: './top-bar.component.scss'
+  styleUrl: './top-bar.component.scss',
 })
 export class TopBarComponent {
-
+  imgUrl = 'https://upload.wikimedia.org/wikipedia/en/0/02/MTG_Arena_Logo.png';
+  constructor(private router: Router) {}
+  navigateHome() {
+    this.router.navigate(['home']);
+  }
 }
